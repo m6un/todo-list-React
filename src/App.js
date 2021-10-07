5d0e58770cb6e648ba6af7c64cb2b634c7af224a
m6un <midhunchandran511@gmail.com>
2021-10-07 21:58:54 +0530

    attributes added

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import TodoForm from './Components/TodoForm';
-import TodoList from './Components/TodoList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
 
-const LOCAL_STORAGE_KEY = "react-todo-list"
+const LOCAL_STORAGE_KEY = "react-todo-list";
 
 function App() {
-
-  const [todos, setTodos] = useState([])
-
-  useEffect(()=>{
-    const storageValues = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if(storageValues){
-      setTodos(storageValues)
-    }
-
-  }, [])
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+    const storageValues = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageValues) {
+      setTodos(storageValues);
+    }
+  }, []);
 
-  const addTodo = (todo)=>{
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
-    setTodos([todo, ...todos])
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  };
 
-  }
-
-  const toggleComplete = (id)=>{
+  const toggleComplete = (id) => {
     setTodos(
-      todos.map(todo=>{
-        if(todo.id === id){
+      todos.map((todo) => {
+        if (todo.id === id) {
           return {
             ...todo,
-            completed : !todo.completed
+            completed: !todo.completed,
           };
         }
         return todo;
       })
-    )
-  }
+    );
+  };
 
-  function removeTodo(id){
-    setTodos(todos.filter((todo) => todo.id !== id))
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
   return (
-    <div className="App">
-      <header className = "App-header">
-        <p>To do List</p>
-        <TodoForm addTodo = {addTodo}/>
-        <TodoList todos = {todos} toggleComplete = {toggleComplete} removeTodo = {removeTodo}/>
-      </header>
-
-
+    <div className = "main">
+      <div className="App">
+        <header className="App-header">
+          <p>To do List</p>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            toggleComplete={toggleComplete}
+            removeTodo={removeTodo}
+          />
+        </header>
+      </div>
+      <div className="footer">
+  <h6>
+    by - <a href="https://github.com/m6un">mi6un_</a> -
+  </h6>
+</div>;
     </div>
   );
 }
 
 export default App;
+
